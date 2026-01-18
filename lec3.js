ae0a2247ccd11d25e344c46c94292f58c032edf5
NILESH KUMAR SETH <aveonlinkdin@gmail.com>
2026-01-18 12:24:58 +0530

    Completed lec 3

---
+++ lec3.js
@@ -0,0 +1,13 @@
+const accountId = 1233
+let accountemail = "nilesh@gmail.com"
+var accoutPass = "213"
+accountCity = "dhanbad"
+
+accountemail = "bfnwub@gmail.com"
+accountCity ="ranchi"
+
+// const cannot be changed
+/// we should no use var -- becsuse of the error of or we can say th eissue 
+// of blockScope and FunctionalScope
+
+console.table([accountId, accountemail,accoutPass,accountCity])
