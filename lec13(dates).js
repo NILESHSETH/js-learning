7b2ae935b53db6a22b07fdd14f1fcaf7c3b9316a
NILESH KUMAR SETH <aveonlinkdin@gmail.com>
2026-01-19 22:25:20 +0530

    Completed lec 8 to 13

---
+++ lec13(dates).js
@@ -0,0 +1,10 @@
+// const now = Temporal.Now.instant();
+// console.log(now.toString());
+
+
+let mydate = new Date()
+
+console.log(new Date().toString());
+console.log(new Date().toDateString());
+console.log(new Date().toLocaleString());
+console.log(typeof new Date());
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,6 @@
-const score = 400
-console.log(score);
+let mydate = new Date()
+
+console.log(new Date().toString());
+console.log(new Date().toDateSting());
+console.log(new Date().toLocalString());
+console.log(typeof new Date())
