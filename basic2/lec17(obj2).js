6fddbdcc17d04af577e6247a54201289ac4cba6a
NILESH KUMAR SETH <aveonlinkdin@gmail.com>
2026-01-20 23:00:29 +0530

    Completed lec 17

---
+++ basic2/lec17(obj2).js
@@ -0,0 +1,16 @@
+// we can crate obj like in two ways ..
+
+//1st 
+const user  = {
+
+}
+//and addig key-value next or later
+//like
+
+user.id = "9764abs"
+user.name= "bciw"
+user.gmail = "couwbvcsodu@.com"
+// console.log(user)
+// uppper obj is not singleton
+
+// now making the single ton obj
