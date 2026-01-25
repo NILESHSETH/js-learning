57b246f3298233a6f5bab1d4f72450c49b47a0e3
NILESH KUMAR SETH <aveonlinkdin@gmail.com>
2026-01-25 15:30:30 +0530

    Completed lec 27

---
+++ lec27(iterations-loops)/one.js
@@ -0,0 +1,18 @@
+// for
+
+for (let i = 0; i < 10; i++) {
+    const k = i;
+    console.log(k);
+}
+
+for(let i = 1; i <= 10; i++){
+    console.log(`the table of: ${i}`);
+    for(let j = 1; j <= 10 ;j++){
+        console.log(`${i} * ${j} = ${i*j}`)
+    }
+}
+
+
+//simply the function of array 
+
+// using break and continue in the loop..-> easy
